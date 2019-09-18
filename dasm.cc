735a8dc9e848b15988e22b97ea5c2e0c8ed5063f
Tamer Aly <taly.dev75@gmail.com>
2019-09-17 22:43:49 -0400

    Remove mention of jumps and visitation.

--- dasm.cc
+++ dasm.cc
@@ -38,28 +38,14 @@ void gbdsm::Disassembler::disassemble()
     while (PC < rom_.size()) {
         const auto& inst = gbdsm::INSTRUCTIONS[rom_[PC]];
         if (inst.length == 0) {
-            // TODO: this is in place while jumps and visitation is not implemented
-            // Without them implemented, we are interpreting data as code
-            // TODO: Again, is it really this easy.
-            std::printf("%.2X @ %.2X is unknown!\n", rom_[PC], PC);
+            // TODO: Is it really this easy?
             PC += 1;
         } 
-        else if (inst.isJump()) {
-            if (inst.op == 0xE9) {
-                // JP (HL) is unsupported as it is a dynamic jump instruction
-                // that cannot easily be traced
-                gbdsm::abort("JP (HL) instruction not supported!\n");
-            }
-            else {
-                print_inst(PC, inst);
-                PC += inst.length;
-            }
-        }
         else if (inst.isPrefix()) {
             const auto& pre = gbdsm::PREFIXED_INSTRUCTIONS[rom_[PC + 1]];
             print_inst(PC, pre);
             PC += pre.length;
-        }  // TODO: implement jumps and visitation
+        }
         else {
             print_inst(PC, inst);
             PC += inst.length;
