40d43ae3b2d2a2b4a56990988d435c7c549c0ad7
Tamer Aly <taly.dev75@gmail.com>
2019-09-22 17:03:10 -0400

    Implement the A8 placeholder.

--- dasm.cc
+++ dasm.cc
@@ -23,6 +23,9 @@ void gbdsm::Disassembler::print_inst(size_t PC, const gbdsm::Instruction& inst) 
         uint16_t target = (int8_t)rom_[PC + 1] + PC + inst.length;
         to_print = to_print.replace(found, 2, to_hex(target));
     }
+    else if ((found = to_print.find("A8")) != std::string::npos) {
+        to_print = to_print.replace(found, 2, to_hex(rom_[PC + 1]));
+    }
     else if ((found = to_print.find("A16")) != std::string::npos) {
         to_print = to_print.replace(found, 3, to_hex(rom_[PC + 1], rom_[PC + 2]));
     }
